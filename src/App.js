9624e89b051a214da37df0254cade7b69871b63c
rohitverma0234 <rohitverma0150@gmail.com>
2023-05-04 13:25:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Home */}
+      <Header/>
+      <Home/>
+
+          {/* Header */}
+
+      {/* Banner */}
+          {/* Search */}
+
+      {/* Cards */}
+          {/* Footer */}
+          <Footer/>
+
+
+          {/* Search Page */}
+          {/*  */}
+    </div>
+  );
+}
+
+export default App;
